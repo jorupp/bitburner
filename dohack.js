28f7aa7e0549317ac6277b51b0ec0cb38ea511cf
Jonathan Rupp <jrupp@rightpoint.com>
2023-12-12 00:38:36 -0600

    update hack script

--- dohack.js
+++ dohack.js
@@ -1,24 +1,27 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    let server = ns.args[0];
-    let minSl = ns.getServerMinSecurityLevel(server);
-    let maxMoney = ns.getServerMaxMoney(server);
-    let targetSl = minSl * 1.1 + 0.1;
-    let targetMoney = maxMoney * 0.8;
-      while(true) {
-      let curSl = ns.getServerSecurityLevel(server);
-      if (curSl > targetSl) {
-        ns.print("running weaken", { curSl, targetSl });
-        await ns.weaken(server);
-        continue;
-      }
-      let curMoney = ns.getServerMoneyAvailable(server);
-      if (curMoney < targetMoney) {
-        ns.print("running grow", { curSl, targetSl, curMoney, targetMoney });
-        await ns.grow(server);
-        continue;
-      }
-      ns.print("running hack", { curSl, targetSl, curMoney, targetMoney });
-      await ns.hack(server);
-      }
-  }
+  const numThreads = 13; // TODO: can we get this dynamically?
+  const targetMoneyAmt = 0.9;
+
+  const server = ns.args[0];
+  const minSl = ns.getServerMinSecurityLevel(server);
+  const maxMoney = ns.getServerMaxMoney(server);
+  const targetSl = minSl * 0.05 * (numThreads + 1);
+  const targetMoney = maxMoney * targetMoneyAmt;
+	while(true) {
+    let curSl = Math.floor(ns.getServerSecurityLevel(server) * 100)/100;
+    if (curSl > targetSl) {
+      ns.print("running weaken", { minSl, curSl, targetSl });
+      await ns.weaken(server);
+      continue;
+    }
+    let curMoney = ns.getServerMoneyAvailable(server);
+    if (curMoney < targetMoney) {
+      ns.print("running grow", { minSl, curSl, targetSl, curMoney, targetMoney, maxMoney });
+      await ns.grow(server);
+      continue;
+    }
+    ns.print("running hack", { minSl, curSl, targetSl, curMoney, targetMoney, maxMoney });
+    await ns.hack(server);
+	}
+}
